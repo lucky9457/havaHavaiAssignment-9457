1268d2437863c9d26f5a333370f455e36908fa5a
lucky9457 <116450098+lucky9457@users.noreply.github.com>
2024-07-05 21:52:25 +0530

    Update Headbar.js

--- src/components/headBar/Headbar.js
+++ src/components/headBar/Headbar.js
@@ -1,7 +1,7 @@
 import { Avatar,Provider, defaultTheme, Grid, View, Heading, Text, Flex, Checkbox, TableView, TableHeader, Column, TableBody, Row, Cell,Button } from '@adobe/react-spectrum';
 import "../Home/home.css"
 const Headbar = ()=>(
-    <Flex alignItems={'center'} UNSAFE_className='containerheading' padding-left= "20px">
+    <Flex height={'100%'}  alignItems={'center'} UNSAFE_className='containerheading' padding-left= "20px">
         <Heading  justifySelf='start'  level={1} UNSAFE_className="logo">hava havai</Heading>
         <Avatar size={'40px'} justifySelf='right' src="https://res.cloudinary.com/dcwxu3d5g/image/upload/v1720157905/havahavai/Avatar_-_Desktop_-_Light_1_piuftp.png" alt="avatar" className='imageclass'/>
         
